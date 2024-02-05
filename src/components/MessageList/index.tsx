cd04a01821004d1b5166d45ca2f12affcdee1b85
2023-089 JoHyunOh <tummy@ecounterp.co.kr>
2024-02-05 09:21:32 +0900

    Feat: add my chat style

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 
 import useRoomInfo from '@/stores/useRoomInfo';
+import useUserInfo from '@/stores/useUserInfo';
 
 const MessageList = () => {
+  const { userInfo } = useUserInfo();
   const { messages } = useRoomInfo();
 
   return (
     <Container>
       {messages.map((message, index) => (
-        <div key={index}>{new Date(message.timestamp).toString()} / {message.user} : {message.message}</div>
+        <ChatMessageBox key={index} className={message.user === userInfo.id? 'me':''}> {new Date(message.timestamp).toString()} / {message.user} : {message.message}</ChatMessageBox>
       ))}
     </Container>
   );
@@ -24,6 +26,22 @@ const Container = styled.div`
   height: 100%;
 
   padding-top: 20px;
+`;
+
+const ChatMessageBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  padding: 10px 20px;
+  margin: 5px 0;
+  border-radius: 10px;
+  background-color: #f0f0f0;
+
+  &.me {
+    align-items: flex-end;
+    background-color: #d3f4ff;
+  }
 `;
 
 export default MessageList;
